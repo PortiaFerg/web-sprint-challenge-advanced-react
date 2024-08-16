098ec0fc2c59d8a7abc8ffdc17c34115313c1ed3
Portia Ferguson <portiaroyalty@gmail.com>
2024-08-16 04:32:28 -0600

    wip

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import AppFunctional from './AppFunctional';
 
 test('Active Square should be index 1 after moving up from index 4', () => {
